c2b785086ae5c5c77fdd3e2281041d69fb56f715
Nelson Sandoval <techednelson@gmail.com>
2022-06-28 18:38:47 +0300

    add nextjs client with material ui

---
+++ apps/client/src/Copyright.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+import MuiLink from '@mui/material/Link';
+
+const Copyright = () => (
+  <Typography variant="body2" color="text.secondary" align="center">
+    {'Copyright © '}
+    <MuiLink color="inherit" href="https://mui.com/">
+      Your Website
+    </MuiLink>{' '}
+    {new Date().getFullYear()}.
+  </Typography>
+);
+
+export default Copyright;
