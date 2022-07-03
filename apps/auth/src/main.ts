ddeb28a7b0784f4794b523671390301c1c3d9d80
Nelson Sandoval <techednelson@gmail.com>
2022-07-04 00:44:26 +0300

    fixes in ci/cd environments

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,11 +1,12 @@
 import 'dotenv/config';
 import mongoose from 'mongoose';
 import app from './app/app';
+import { environment } from './environments/environment';
 
 const start = async () => {
   try {
-    // await mongoose.connect('mongodb://auth-mongo-service:27017/auth');
-    await mongoose.connect('mongodb://localhost:27017/auth');
+    await mongoose.connect('mongodb://auth-mongo-service:27017/auth');
+    // await mongoose.connect(environment.dbURL);
     console.log('connected to MongoDB');
 
     const port = process.env.port || 3333;
---
+++ infra/k8s-dev/build-docker-images.sh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+set -o allexport
+source .env set
++o allexport
+
+minikube start
+
+minikube docker-env
+
+# shellcheck disable=SC2046
+eval $(minikube -p minikube docker-env)
+
+docker build -f apps/auth/Dockerfile . -t "$DOCKER_USERNAME"/auth
+docker build -f apps/client/Dockerfile . -t "$DOCKER_USERNAME"/client
+
+kubectl apply -f infra/k8s/auth-mongo-deployment.yml
+
+
+kubectl apply -f infra/k8s-dev/auth-deployment.yml
+kubectl apply -f infra/k8s-dev/client-deployment.yml
+
+kubectl apply -f infra/k8s-dev/ingress-service.yml
