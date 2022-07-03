29abfdb52ade5b88f1d80ce7c480d48f1823f315
Nelson Sandoval <techednelson@gmail.com>
2022-07-03 11:12:08 +0300

    fix local env for minikube

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -4,7 +4,8 @@ import app from './app/app';
 
 const start = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/auth');
+    await mongoose.connect('mongodb://auth-mongo-service:27017/auth');
+    // await mongoose.connect('mongodb://localhost:27017/auth');
     console.log('connected to MongoDB');
 
     const port = process.env.port || 3333;
